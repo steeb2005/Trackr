0b899641fb83ae92f0a467f52e9243a8f53f5c3f
steeb2005 <anenonjohnsteven3a@gmail.com>
2026-05-01 17:01:40 +0800

    maybe final adjustment

--- server/server.js
+++ server/server.js
@@ -7,8 +7,10 @@ const mongoose = require('mongoose');
 const app = express();
 
 // Middleware
+// Change CORS_ORIGIN for development
+
 app.use(cors({
-  origin: process.env.CORS_ORIGIN || 'http://localhost:3000',
+  origin: process.env.CORS_ORIGIN || 'http://localhost:5173',
   credentials: true
 }));
 
